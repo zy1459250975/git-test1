8c3ae2dea7b0b72e3ab26761c45c414261573340
zy1459250975 <113323967+zy1459250975@users.noreply.github.com>
2022-09-12 11:43:19 +0800

    pull commit

--- src/main/java/com/atguigu/test/Gittest.java
+++ src/main/java/com/atguigu/test/Gittest.java
@@ -10,5 +10,6 @@ public class Gittest {
         System.out.println("只是git2");
         System.out.println("只是git2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
