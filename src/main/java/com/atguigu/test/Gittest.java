981195e519f362958a3a38a1bee063cf11cfbdcb
Zy154215 <145925@atguigu.com>
2022-09-12 10:14:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/test/Gittest.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author zystart
+ * @create 2022-@{MONTH}-@{DAY} @{TIME}
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("只是git");
+    }
+}
