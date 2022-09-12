546d3154ded765ad7eeea55d15a97dab37a8d47b
Zy154215 <145925@atguigu.com>
2022-09-12 10:58:59 +0800

    hot-fix5 commit4

--- src/main/java/com/atguigu/test/Gittest.java
+++ src/main/java/com/atguigu/test/Gittest.java
@@ -8,5 +8,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("只是git");
         System.out.println("只是git2");
+        System.out.println("只是git2");
     }
 }
